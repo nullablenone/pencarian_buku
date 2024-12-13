24291608af348e21005cc053af7d3f353b48b81b
useryesa9@gmail.com <useryesa9@gmail.com>
2024-12-13 15:32:57 +0700

    membuat section dan membuat Component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
+import FormSearch from './components/FormSearch.vue';
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <Navbar />
+
+  <section class="flex flex-col items-center my-12">
+    <h1 class="text-xl md:text-2xl: font-semibold mb-6">Temukan Buku Yang Kamu Mau</h1>
+    <FormSearch />
+  </section>
 </template>
---
+++ src/components/FormSearch.vue
@@ -0,0 +1,10 @@
+<template>
+  <form action="" class="space-x-2">
+    <input
+      type="search"
+      class="w-[60vw] rounded-md border border-gray-300 py-2 px-4 focus:outline-none focus-within:ring-zinc-900 focus:border-zinc-900"
+      placeholder="tipe buku nama, pengarang..."
+    />
+    <button class="py-2 px-4 rounded-md bg-zinc-900 text-zinc-100 hover:bg-zinc-800">Cari</button>
+  </form>
+</template>
