c6d77dfffc4afe7124003a4f9f55b3122832dcb9
useryesa9@gmail.com <useryesa9@gmail.com>
2024-12-13 14:53:39 +0700

    membuat Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import Navbar from './components/Navbar.vue'
+</script>
 
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="bg-gray-800 p-4">
+    <div class="container mx-auto flex justify-between items-center">
+      <!-- Logo -->
+      <a href="#" class="text-white text-xl font-bold">Pencari Buku</a>
+    </div>
+  </nav>
+</template>
